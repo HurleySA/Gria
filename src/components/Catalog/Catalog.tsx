03f4d1c11a1b69c259228a6de4c1881ae74bde56
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2021-12-18 16:22:51 -0300

    Catalogo e Iniciando - Loading e  paginação

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,48 +1,20 @@
-import { Avatar, Box, Container, Grid, Typography } from "@mui/material"
+import { Avatar, Box, Container, Grid, Link, Pagination, Typography } from "@mui/material"
 import { makeStyles } from "@mui/styles"
 import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import { useEffect, useState } from "react";
 
-const vagas = [
-    {id:1,
-     nameCompany: "BRASIL WEALTH MANAGEMENT GESTAO DE PATRIMONIO LTDA",
-     srcLogo: "https://avatars.githubusercontent.com/u/29778166?v=4",
-     nameJob: "ADVOGADO JUNIOR - Tributário Pessoa Física",
-     position: "Júnior",
-     period: "Integral",
-     location: "Rio De Janeiro, Rio De Janeiro, Brasil",
-     date: new Date(2021, 11, 16),
-    },
-    {id:2,
-        nameCompany: "BRASIL WEALTH MANAGEMENT GESTAO DE PATRIMONIO LTDA",
-        srcLogo: "https://avatars.githubusercontent.com/u/29778166?v=4",
-        nameJob: "ADVOGADO JUNIOR - Tributário Pessoa Física",
-        position: "Júnior",
-        period: "Integral",
-        location: "Rio De Janeiro, Rio De Janeiro, Brasil",
-        date: new Date(2021, 11, 11),
-       },
-
-    {id:3,
-     nameCompany: "BRASIL WEALTH MANAGEMENT GESTAO DE PATRIMONIO LTDA",
-     srcLogo: "https://avatars.githubusercontent.com/u/29778166?v=4",
-     nameJob: "ADVOGADO JUNIOR - Tributário Pessoa Física",
-     position: "Júnior",
-     period: "Integral",
-     location: "Rio De Janeiro, Rio De Janeiro, Brasil",
-     date: new Date(2021, 11, 1),
-    },
-    {id:4,
-        nameCompany: "BRASIL WEALTH MANAGEMENT GESTAO DE PATRIMONIO LTDA",
-        srcLogo: "https://avatars.githubusercontent.com/u/29778166?v=4",
-        nameJob: "ADVOGADO JUNIOR - Tributário Pessoa Física",
-        position: "Júnior",
-        period: "Integral",
-        location: "Rio De Janeiro, Rio De Janeiro, Brasil",
-        date: new Date(2021, 11, 16),
-       }
-]
+interface vagasProps{
+    id: number;
+    nameCompany: string;
+    srcLogo: string;
+    nameJob: string;
+    position: string;
+    period: string;
+    location: string;
+    date: Date;
+}
 
 const useStyles = makeStyles({
     totalJobsH2:{
@@ -52,17 +24,50 @@ const useStyles = makeStyles({
         letterSpacing:'0.01px', 
         color:'#263238'
     },
+    gridItem: {
+        maxWidth:'300px',
+        
+        '&:hover':{
+            cursor:'pointer',
+        },
+    },
     teste:{
         padding:"2rem 0rem",
         color:'#263238',
         letterSpacing:'0.01px',
-    }
+    },
+    pagination: {
+        "&":{
+            marginTop: "4rem",
+            marginBottom: "2rem",
+        },
+        "& > *": {
+          justifyContent:"center",
+          display:'flex'
+        }
+      }
+    
 })
-
-const diasVagas = vagas[0].date.getTime() / (24*60*60*1000);
-const diasNow = Date.now() / (24*60*60*1000);
-console.log(Math.ceil(diasNow) - Math.ceil(diasVagas));
+const getDaysDiff = (data: Date) =>{
+    const daysPostedJob = new Date(data).getTime() / (24*60*60*1000);
+    const diasNow = Date.now() / (24*60*60*1000);
+    return (Math.ceil(diasNow) - Math.ceil(daysPostedJob));
+}
 export default function Catalog() {
+    const [vagas, setVagas] = useState<vagasProps[]>([]);
+    const [loading, setLoading] = useState<Boolean>(true);
+    const [page, setPage] = useState<number>(2);
+
+    useEffect(()=> {
+        const url = "http://localhost:8000/vagas";
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            setVagas(json)
+            setLoading(false);
+        })
+    },[])
+
     const classes = useStyles();
     return (
         <Container maxWidth="lg">
@@ -70,47 +75,55 @@ export default function Catalog() {
                 Encontramos {vagas.length} oportunidades cadastradas
             </Typography>
             <Grid container spacing={4}>
-                    <Grid item lg={3} md={4} sm={6} xs={9} sx={{maxWidth:"300px"}}>
-                        <Box  sx={{padding:'1rem', boxShadow:'0px 2px 2px rgb(0 0 0 / 10%)'}}>
-                            <Box sx={{display:'flex',alignItems:'center'}}>
-                                <Avatar alt="Remy Sharp" src={vagas[0].srcLogo} sx={{marginRight:'10px'}}/>
-                                <Typography variant="body1" component="div" sx={{maxWidth:"180px", height:"60px", fontSize:".8rem", fontWeight:"300", lineHeight:".8rem"}} >
-                                {vagas[0].nameCompany}
-                                </Typography>
-                            </Box>
-                            <Typography variant="body1" component="div" sx={{fontSize:".8rem", fontWeight:'bold', padding:".7rem 0rem 1.5rem 0rem"}}>
-                                {vagas[0].nameJob}
+                {vagas.map(vaga => (
+                    <Grid item lg={3} md={4} sm={6} xs={9} key={vaga.id} sx={{maxWidth:"300px"}}>
+                    <Link href="https://www.gria.io/" underline="none" color="inherit" sx={{padding:"1rem",boxShadow:'0px 2px 2px rgb(0 0 0 / 10%)', height:"100%"}} className={classes.gridItem}>
+                        <Box sx={{display:'flex',alignItems:'center'}}>
+                            <Avatar alt="Remy Sharp" src={vaga.srcLogo} sx={{marginRight:'10px'}}/>
+                            <Typography variant="body1" component="div" align="center" sx={{maxWidth:"180px", height:"60px", fontSize:".8rem", fontWeight:"300", lineHeight:".8rem"}} >
+                            {vaga.nameCompany}
                             </Typography>
-
-                            <Grid container direction="row" alignItems="center" spacing={1}>
-                                <Grid item>
-                                    <PersonSearchIcon fontSize="small" sx={{color: '#919090'}}/>
-                                </Grid>
-                                <Grid item sx={{fontSize:".7rem", fontWeight:"300", lineHeight:".8rem"}}>
-                                    {vagas[0].position}
-                                </Grid>
+                        </Box>
+                        <Box sx={{height:"60px"}}>
+                        <Typography variant="body1" component="div" sx={{fontSize:".8rem", fontWeight:'bold', padding:".7rem 0rem 0rem 0rem"}}>
+                            {vaga.nameJob}
+                        </Typography>
+                        </Box>   
+                        <Grid container direction="row" alignItems="center" spacing={1}>
+                            <Grid item>
+                                <PersonSearchIcon fontSize="small" sx={{color: '#919090'}}/>
                             </Grid>
-                            <Grid container direction="row" alignItems="center" spacing={1}>
-                                <Grid item>
-                                    <AccessAlarmIcon fontSize="small" sx={{color: '#919090'}}/>
-                                </Grid>
-                                <Grid item sx={{fontSize:".7rem", fontWeight:"300", lineHeight:".8rem"}}>
-                                    {vagas[0].period}
-                                </Grid>
+                            <Grid item sx={{fontSize:".7rem", fontWeight:"300", lineHeight:".8rem"}}>
+                                {vaga.position}
                             </Grid>
-                            <Grid container direction="row" alignItems="center" spacing={1}>
-                                <Grid item>
-                                    <LocationOnOutlinedIcon fontSize="small" sx={{color: '#919090'}}/>
-                                </Grid>
-                                <Grid item sx={{fontSize:".7rem", fontWeight:"300", lineHeight:".8rem"}}>
-                                    {vagas[0].location}
-                                </Grid>
+                        </Grid>
+                        <Grid container direction="row" alignItems="center" spacing={1}>
+                            <Grid item>
+                                <AccessAlarmIcon fontSize="small" sx={{color: '#919090'}}/>
                             </Grid>
-                            
-                         </Box>
-                     </Grid>
+                            <Grid item sx={{fontSize:".7rem", fontWeight:"300", lineHeight:".8rem"}}>
+                                {vaga.period}
+                            </Grid>
+                        </Grid>
+                        <Grid container direction="row" alignItems="center" spacing={1}>
+                            <Grid item>
+                                <LocationOnOutlinedIcon fontSize="small" sx={{color: '#919090'}}/>
+                            </Grid>
+                            <Grid item sx={{fontSize:".7rem", fontWeight:"300", lineHeight:".8rem"}}>
+                                {vaga.location}
+                            </Grid>
+                        </Grid>
+                        <Typography variant="body1" component="div" sx={{fontSize:".7rem", margin:"2.5rem  0rem 0rem 0rem"}}>
+                            Criada há {getDaysDiff(vaga.date)} dias
+                        </Typography>
+                     </Link>
+                 </Grid>
+                ))}
+                   
                 
             </Grid>
+
+            <Pagination count={Math.ceil(vagas.length/3)} color="secondary" className={classes.pagination} onChange={(event, page) => console.log(page)} />
         </Container>
     )
 }
